e614503db333a60f2ae80fb3fcdf3efe6e0d7ad4
Maowcraft <aidancrafter@gmail.com>
2021-04-17 13:40:33 -0400

    injector changes

--- example/src/main/java/org/transparent/interlucent/example/ExampleTranslator.java
+++ example/src/main/java/org/transparent/interlucent/example/ExampleTranslator.java
@@ -1,25 +1,17 @@
 package org.transparent.interlucent.example;
 
 import com.sun.tools.javac.code.TypeTag;
-import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.*;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Names;
 import org.transparent.interlucent.transform.EurekaTranslator;
 import org.transparent.lucent.transform.LucentValidator;
 
-import javax.lang.model.element.Element;
-
 import static org.transparent.eureka.util.Modifiers.PRIVATE_FINAL;
 
 public final class ExampleTranslator extends EurekaTranslator {
     public ExampleTranslator(Names names, TreeMaker factory, LucentValidator validator) {
         super(names, factory, validator);
-    }
-
-    @Override
-    public void translate(JCTree tree, Element element) {
-        tree.accept(this);
     }
 
     @Override
--- src/main/java/org/transparent/interlucent/util/Injector.java
+++ src/main/java/org/transparent/interlucent/util/Injector.java
@@ -5,6 +5,8 @@ import com.sun.tools.javac.tree.JCTree.*;
 import org.transparent.eureka.impl.builder.FieldBuilder;
 import org.transparent.eureka.impl.builder.MethodBuilder;
 
+import java.util.Collection;
+
 public abstract class Injector<T extends JCTree, E extends JCTree> {
     protected final T tree;
 
@@ -14,13 +16,22 @@ public abstract class Injector<T extends JCTree, E extends JCTree> {
 
     public abstract Injector<T, E> add(E tree);
 
+    public abstract Injector<T, E> add(Collection<E> trees);
+
     public T tree() {
         return tree;
     }
 
     public static class MemberInjector extends Injector<JCClassDecl, JCTree> {
+        private Class<?> filterClass;
+
         public MemberInjector(JCClassDecl tree) {
             super(tree);
+        }
+
+        public MemberInjector filter(Class<?> filterClass) {
+            this.filterClass = filterClass;
+            return this;
         }
 
         public MemberInjector add(MethodBuilder builder) {
@@ -33,8 +44,17 @@ public abstract class Injector<T extends JCTree, E extends JCTree> {
 
         @Override
         public MemberInjector add(JCTree tree) {
+            if (filterClass != null
+                    && !filterClass.isInstance(tree))
+                return this;
             this.tree.defs = this.tree.defs
                     .append(tree);
+            return this;
+        }
+
+        @Override
+        public MemberInjector add(Collection<JCTree> trees) {
+            trees.forEach(this::add);
             return this;
         }
     }
@@ -48,6 +68,12 @@ public abstract class Injector<T extends JCTree, E extends JCTree> {
         public StatementInjector add(JCStatement statement) {
             this.tree.body.stats = this.tree.body.stats
                     .append(statement);
+            return this;
+        }
+
+        @Override
+        public Injector<JCMethodDecl, JCStatement> add(Collection<JCStatement> trees) {
+            trees.forEach(this::add);
             return this;
         }
     }
